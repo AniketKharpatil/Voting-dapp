32e6da3335336e6b7d873d7f0964c760a1dbfe0b
AniketKharpatil <aniketkharpatil@gmail.com>
2022-10-27 14:47:58 +0530

    infura url updated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,7 @@
-String infura_url =
-    "https://kovan.infura.io/v3/7e0fcba16fce454c96616d04abdbf3a4";
+String infura_url ="https://sepolia.infura.io/v3/7b003b44b5f946c196c1a5ea9194a72f";
 
-String owner_private_key =
-    "9e2c82ef68ad0baf4bc0fa84bbdc84e39e5f9a49d874cb635f895985ea45d2f8";
+String owner_private_key ="d5e84ec45fe5555c22f255621d286f50d0320ac2e833a915a9c79d295f4cbb21";
 
-String voter_private_key =
-    "0007a74d6b0385232ac4722b936742613ba9fb6f57384dcee69a4769320dc6cc";
+String voter_private_key ="b06e2c6f9095b25f0bbdc0af01dcf7baf18d4b576c6ccfaf33a4cd4adce85ce3";
 
 String contractAddress1 = "0x47891C0f57e38FfFFb7A08C5A81193b5D4471AdD";
